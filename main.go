b637ff2c753950f28649cf6d1342f757ceab03a9
Edgar Erenberg <EdgarErenbergs@rambler.ru>
2023-12-25 18:25:36 +0300

    Updated

--- main.go
+++ main.go
@@ -5,28 +5,60 @@ import (
 	"math"
 )
 
+const inflationRate float64 = 2.5
+
 func main() {
 
-	const inflationRate float64 = 2.5
-
 	var (
-		investmentAmount int
-		expectedReturnRate float64
-		years int
+		investmentAmount,
+		expectedReturnRate,
+		years float64
 	)
 
-	fmt.Print("Investment Amount: ")
+	// ----------------------- Input parameters
+
+	printText("Investment Amount: $ ")
 	fmt.Scan(&investmentAmount)
 
-	fmt.Print("Expected Return Rate: ")
+	printText("Expected Return Rate: % ")
 	fmt.Scan(&expectedReturnRate)
 
-	fmt.Print("Years: ")
+	printText("Years: ")
 	fmt.Scan(&years)
 
-	futureValue := float64(investmentAmount) * math.Pow((1 + expectedReturnRate / 100), float64(years))
-	futureRealValue := futureValue / math.Pow(1 + inflationRate / 100, float64(years))
+	// ----------------------- Calculation
 
-	fmt.Println("Future Value: ", futureValue)
-	fmt.Println("Future Real Value: ", futureRealValue)
+	futureValue, futureRealValue := calculateFutureValues(investmentAmount, expectedReturnRate, years)
+
+	// ----------------------- Formatting
+
+	formattedFV := fmt.Sprintf("Future Value: $ %.1f\n", futureValue)
+	formattedRFV := fmt.Sprintf("Future Value (AFI): $ %.1f\n", futureRealValue)
+
+	// ----------------------- Outputs
+
+	printOutputText(formattedFV, formattedRFV)
+	printAditionalInfo()
+}
+
+func printText (text string) {
+	fmt.Print(text)
+}
+
+func calculateFutureValues (investmentAmount, expectedReturnRate, years float64) (float64, float64) {
+	fV := investmentAmount * math.Pow((1 + expectedReturnRate / 100), years)
+	fRV := fV / math.Pow(1 + inflationRate / 100, years)
+	return fV, fRV
+}
+
+func printOutputText (firstText, secondText string) {
+	fmt.Println("======================== Result:")
+	fmt.Print(firstText, secondText)
+}
+
+func printAditionalInfo () {
+	fmt.Print("\n")
+	fmt.Println("======================== Acronyms:")
+	fmt.Println("AFI - Adjusted for inflation")
+	fmt.Print("\n")
 }
