2a89f619516c89f977d17c3d749ee175753090fa
Edgar Erenberg <EdgarErenbergs@rambler.ru>
2023-12-25 23:51:04 +0300

    Updated

--- main.go
+++ main.go
@@ -45,10 +45,10 @@ func printText (text string) {
 	fmt.Print(text)
 }
 
-func calculateFutureValues (investmentAmount, expectedReturnRate, years float64) (float64, float64) {
-	fV := investmentAmount * math.Pow((1 + expectedReturnRate / 100), years)
-	fRV := fV / math.Pow(1 + inflationRate / 100, years)
-	return fV, fRV
+func calculateFutureValues (investmentAmount, expectedReturnRate, years float64) (fV float64, fRV float64) {
+	fV = investmentAmount * math.Pow((1 + expectedReturnRate / 100), years)
+	fRV = fV / math.Pow(1 + inflationRate / 100, years)
+	return
 }
 
 func printOutputText (firstText, secondText string) {
